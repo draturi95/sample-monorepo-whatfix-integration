43180d22a9145d08e2ad1f15b6adde9a7cc3546f
Dhawal Raturi <dhawalraturi@Dhawals-MacBook-Pro.local>
2022-11-16 18:57:13 +0530

    feat: Whatfix integration in Expo app

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,7 +1,22 @@
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 
+import LeapReactNative from 'leap-react-native'
+import LeapCreatorReactNative from 'leap-creator-react-native'
+import { Platform } from 'react-native'
+import { useEffect } from 'react'
+
+const WHATFIX_API_KEY = ''
 export default function App() {
+  useEffect(() => {
+    Platform.select({
+      android: () => {
+        LeapReactNative.start(WHATFIX_API_KEY)
+        LeapCreatorReactNative.start(WHATFIX_API_KEY)
+      },
+    })()
+  }, [])
+
   return (
     <Provider>
       <NativeNavigation />
